b76844e2ed533afe5662d971d96d8f9a4fbc4d10
bbouley <b.bouley@hotmail.co.uk>
2015-07-29 14:39:44 -0600

    added some functioniality that edited the DOM

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  var $heading = $('#first-heading');
+  console.log($heading);
+
+  $heading.append('<h3>Testing</h3>');
+
+  var test = $('.container p').first();
+  var test2 = $('p:eq(3)').append('&nbsp;&nbsp;<span>appending</span>');
+  var testClone = $heading.clone();
+  console.log(testClone);
+
+  var testClone2 = $('#delegate').clone();
+  console.log(testClone2);
+
+  $("#main-btn").on('click', function(){
+    console.log('hello, is it me you\'re looking for?');
+    $heading.append('<h3>Blah Blah Blah</h3>');
+    console.log(this);
+  });
+
 });
